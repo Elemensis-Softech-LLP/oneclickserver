a035fcaff866d7d01f6ca57db0fd1ab7fc629894
Christopher Arguello <christopheraarguello@gmail.com>
2018-09-21 18:40:21 -0400

    Create coin posts to DB, and rendered in coins/index

--- models/coin.js
+++ models/coin.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const Schema   = mongoose.Schema;
 
 const coinSchema = new Schema({
-  name:  { type: String, required: true, unique: true },
-  ticker: { type: String, required: true, unique: true },
+  coinName:  { type: String, required: true, unique: true },
+  coinTicker: { type: String, required: true, unique: true },
   supported: {type: Boolean, default: false}
 }, {
   timestamps: {
--- routes/coins.js
+++ routes/coins.js
@@ -7,4 +7,26 @@ coinsRouter.get('/coins/create', function(req, res, next) {
    res.render('coins/create') 
 });
 
+coinsRouter.post('/coins/create', function(req, res, next) {
+    const coinName = req.body.coinName;
+    const coinTicker = req.body.coinTicker;
+    const newCoin = new Coin({
+        coinName: coinName,
+        coinTicker: coinTicker
+    })
+    newCoin.save();
+    res.redirect('/coins')
+ });
+
+coinsRouter.get('/coins', function(req, res, next) {
+    Coin.find({}, function(err, data){
+        if(err) {
+          res.render('error')
+        } else {
+          console.log(data[0].coinName)
+          res.render('coins/index', { "coins": data });
+        }
+      });
+ });
+
 module.exports = coinsRouter;
