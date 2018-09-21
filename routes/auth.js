d2b2c630ff2a2cf5473952478287491d16795908
Christopher Arguello <christopheraarguello@gmail.com>
2018-09-21 18:10:45 -0400

    Create Signup methods, pw using bcrypt

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,46 @@
 var express = require('express');
 var authRouter = express.Router();
 
-// const authRoutes     = express.Router();
+const User = require('../models/user');
 
-/* GET users listing. */
+const bcrypt = require("bcrypt");
+
+// Get Login page
 authRouter.get('/login', function(req, res, next) {
   res.render('auth/login');
 });
 
+// Get Signup page
 authRouter.get('/signup', function(req, res, next) {
     res.render('auth/signup');
 });
+
+// Create new User from signup
+authRouter.post('/signup', function(req, res, next){
+  console.log(req.body.password)
+  const email = req.body.email;
+  const password = req.body.password;
+  const hashedPassword = generateHashedPassword(password);
+  const newUser = new User({
+    email: email,
+    password: password
+  })
+  newUser.save();
+  res.redirect('/success');
+})
+
+//GENERATE HASHED PASSWORD
+function generateHashedPassword(password) {
+  let hashPass;
+  const bcryptSalt = 10;
+  const salt = bcrypt.genSaltSync(bcryptSalt);
+  hashPass = bcrypt.hashSync(password, salt);
+  return hashPass;
+}
+
+
+
+
+
 
 module.exports = authRouter;
