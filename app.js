4e005dfb71053619c150d0060ec9bfbebc7ab5ee
Christopher Arguello <christopheraarguello@gmail.com>
2018-09-24 14:58:28 -0400

    Add npm packs: passport, passport-local, express-session

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -5,12 +7,10 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser')
 var logger = require('morgan');
 var mongoose = require("mongoose");
-require('dotenv').config();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var authRouter = require('./routes/auth');
 var coinsRouter = require('./routes/coins');
-
 
 var app = express();
 
